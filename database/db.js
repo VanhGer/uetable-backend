ec24b5bb458a2579aa0456dd5afe774078093baa
Phạm Gia Việt Anh <vietanhpg2003@gmail.com>
2023-10-18 18:33:01 +0700

    "connect database: done"

---
+++ database/db.js
@@ -0,0 +1,10 @@
+import { createConnection } from 'mysql';
+
+const connection = createConnection({
+    host: "brghur5oywbwebq7w3ml-mysql.services.clever-cloud.com",
+    user: "ulhs6n9bga1x3bwd",
+    password: "PjVebpU0Bm7iit3jkOP3",
+    database: "brghur5oywbwebq7w3ml"
+});
+
+export default connection;
