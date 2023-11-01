917cf7cee76c357a385b1136232adb94e6cb5d36
Phạm Gia Việt Anh <vietanhpg2003@gmail.com>
2023-11-01 14:32:43 +0700

    "add create Document API"

--- controllers/document.js
+++ controllers/document.js
@@ -3,11 +3,34 @@ import Document from "../models/document.js";
 
 export const createDocument = async (req, res) => {
     try {
-      
-        console.log(req.files);
-        console.log(req.body);
-        res.status(200).json("ok");
+        const body = JSON.parse(req.body.request);
+        for (let file of req.files) {
+            let newDocument = await Document.create({
+                Name: body.name,
+                Category: body.category,
+                Link: file.path,
+                UserId: body.userId,
+                SubjectId: body.subjectId
+            });
+            await newDocument.save();
+        }
+        res.status(200).json("Create successfully");
     } catch (err) {
         res.status(500).json(err.message);
     }
-};
+}; 
+
+export const getDocumentById = async (req, res) => {
+    try {
+        const docId = req.body.id;
+        const doc = await Document.findOne({
+            where: {
+                Id: docId
+            }
+        }) 
+        res.status(200).json(doc);
+        //res.status(200).json("Create successfully");
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+}  
